6bd46294091034053ec5f2f54ced2f19a3c187b5
QuentinVerkler <43121166+QuentinVerkler@users.noreply.github.com>
2019-09-11 10:20:13 -0700

    asdf

---
+++ Labs/Lab 0911 - First Animation With JSVector/animation.js
@@ -0,0 +1,51 @@
+window.onload = init;
+var cnv;
+var ctx;
+
+var loc = [[]];
+var vel = [[]];
+var radius = [];
+
+
+function init(){
+	//get canvas
+	cnv = document.getElementById('cnv');
+	//set dimensions
+	cnv.width = window.innerWidth;
+	cnv.height = window.innerHeight;
+	cnv.style.border = 'solid black 2x';
+	cnv.style.backgroundColor = 'rgba(0,44,55, .5)';
+	//get context
+	ctx = cnv.getContext('2d');
+	for(let a = 0; a < 5; a++){
+		radius[a] = Math.Random * 20 + 10;
+		loc[a] = new JSVector(Math.random() * (window.innerWidth - 2*radius) + radius, Math.random() * (window.innerHeight - 2*radius) + radius);
+		vel[a] = new JSVector(Math.random() * 6 - 3, Math.random() * 6 - 3);
+	}
+	animate();
+}
+
+/*
+var loc, vel, radius;
+radius = 30;
+loc = new JSVector(Math.random() * (window.innerWidth - 2*radius) + radius, Math.random() * (window.innerHeight - 2*radius) + radius);
+vel = new JSVector(Math.random() * 6 - 3, Math.random() * 6 - 3);
+*/
+
+function animate(){
+	requestAnimationFrame(animate);
+	ctx.clearRect(0,0,window.innerWidth, window.innerHeight);
+
+	ctx.strokeStyle = 'rgb(255,105,180)';
+	ctx.fillStyle = 'rgb(255,105,180)';
+	for(let a = 0; a < 5; a++){
+		ctx.beginPath();
+		ctx.arc(loc[a].x, loc[a].y, radius, Math.PI*2, 0, false);
+		ctx.fill();
+		ctx.stroke();
+		loc[a].add(vel[a]);
+		
+		if(loc[a].x < radius || loc[a].x > window.innerWidth - radius) vel[a].x = -vel[a].x;
+		if(loc[a].y < radius || loc[a].y > window.innerHeight - radius) vel[a].y = -vel[a].y;
+	}
+}
