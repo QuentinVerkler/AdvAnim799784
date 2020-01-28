2ccbf228721677d8dde3732c974efc89c84e4656
QuentinVerkler <43121166+QuentinVerkler@users.noreply.github.com>
2020-01-28 10:25:17 -0800

    asdf

--- Labs/Lab 0122 - Physics Engine/airFriction.js
+++ Labs/Lab 0122 - Physics Engine/airFriction.js
@@ -7,10 +7,12 @@ var Engine = Matter.Engine,
     Runner = Matter.Runner,
     Composite = Matter.Composite,
     Composites = Matter.Composites,
+    Constraint = Matter.Constraint,
     Common = Matter.Common,
     MouseConstraint = Matter.MouseConstraint,
     Mouse = Matter.Mouse,
     World = Matter.World,
+    Events = Matter.Events,
     Bodies = Matter.Bodies;
 
 // create an engine
@@ -26,12 +28,17 @@ var render = Render.create({
       height: 1000,
       showVelocity: true,
       showAngleIndicator: true,
-      showCollisions: true
+      showCollisions: true,
+      background: '#111'
     }
 });
 
 // run the renderer
 Render.run(render);
+
+//create Runner
+// var runner = Runner.create();
+// Runner.run(runner, engine);
 
 //mouse control
 var mouse = Mouse.create(render.canvas),
@@ -52,26 +59,38 @@ var purpleColor = '#751575';
 
 // create two boxes and a ground
 var boxA = Bodies.rectangle(400, 200, 80, 80,
-  {density: .1, frictionAir: .07, friction: .01});
+  {density: .1, frictionAir: .07, friction: .01,});
 var boxB = Bodies.rectangle(450, 50, 80, 80,
   {density: .2, frictionAir: .03, friction: .7});
-var circA = Bodies.circle(320, 150, 60, 1, 50, 10,
-  {density: .4, frictionAir: .05, friction: .9});
+var circA = Bodies.circle(280, 150, 60,
+  {density: .3, restitution: .9, frictionAir: .03, friction: .9});
 var ground1 = Bodies.rectangle(300, 410, 810, 60,
   { isStatic: true, angle: Math.PI * .09 });
 var ground2 = Bodies.rectangle(700, 750, 400, 60,
   { isStatic: true, angle: -Math.PI * .05 });
 var ground3 = Bodies.rectangle(380, 800, 200, 40,
   { isStatic: true, angle: Math.PI * .5 });
-var circB = Bodies.circle(380, 600, 90, 1, 500, 20,
+var circB = Bodies.circle(380, 640, 60,
   {
     render: {
       fillStyle: purpleColor
     }
   });
 
+
 // add all of the bodies to the world
-World.add(engine.world, [boxA, boxB, circA, circB, ground1, ground2, ground3]);
+World.add(world, [boxA, boxB, circA, circB, ground1, ground2, ground3]);
+
+//adds rope
+World.add(world, Constraint.create({
+  pointA: {x: 380, y: 470},
+  bodyB: circB
+}));
+
+World.add(world, Constraint.create({
+  pointA: {x: 300, y: 150},
+  bodyB: boxB
+}));
 
 // run the engine
 Engine.run(engine);
